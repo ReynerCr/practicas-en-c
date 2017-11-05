df6e0d9eb444931eb7dce71b97771a94f01de3a4
reynercr <reynercon@outlook.com>
2017-11-05 14:00:45 -0400

    Arreglos de código y bastante avance en el mismo.

--- Desarrollo/Letras_en_movimiento.c
+++ Desarrollo/Letras_en_movimiento.c
@@ -3,36 +3,132 @@
 #include <conio.h>
 #include <string.h>
 #include <ctype.h>
-#define T 100
+#include <windows.h>
+#define t 100
+//LIBRERIAS Y DEFINE
+//-----------------------------------------
 
-void letras (int letra);
+
+void gotoxy( int column, int line ) { //GOTOXY
+  COORD coord;
+  coord.X = column;
+  coord.Y = line;
+  SetConsoleCursorPosition(
+    GetStdHandle( STD_OUTPUT_HANDLE ),
+    coord
+  );
+}
+
+int wherex() {
+  CONSOLE_SCREEN_BUFFER_INFO csbi;
+  COORD                      result;
+  if (!GetConsoleScreenBufferInfo(
+         GetStdHandle( STD_OUTPUT_HANDLE ),
+         &csbi
+     ))
+    return -1;
+  return result.X;
+}
+
+int wherey() {
+  CONSOLE_SCREEN_BUFFER_INFO csbi;
+  COORD                      result;
+  if (!GetConsoleScreenBufferInfo(
+         GetStdHandle( STD_OUTPUT_HANDLE ),
+         &csbi
+     ))
+    return -1;
+  return result.Y;
+}
+//FIN GOTOXY
+//-----------------------------------------
+
+
+void letras (int unsigned letra[t], char unsigned cadena[t]);
+void imprimir (int mat[5][5], int i, int j);
+void switches (char unsigned var, int mat[5][5], int i);
+void copiarmat (int original[5][5], int copia[5][5], int i);
+//PROTOTIPOS
+//-----------------------------------------
+
 
 int main () {
-	char unsigned cadena[T];
-	int unsigned letra, i;
+	char unsigned cadena[t];
+	int unsigned letra[t], i;
 	printf ("Ingrese lo que quiera ver: \n");
 	strset (cadena, '\0');
 	fflush (stdin);
 	gets (cadena);
 	system ("cls");
 	
-	for (i=0; i<T; i++) {
+	for (i=0; i<t; i++) {
 		if (cadena[i]=='\0') {
 				break;
 		}
 		if ((cadena[i]>=65 && cadena[i]<=90) || (cadena[i]==164 || cadena[i]==165)) {
-			letra=cadena[i];
+			letra[i]=cadena[i];
 		}
 	}
-	letras(letra);
-	printf ("%s", cadena);
+	letras(letra, cadena);
+	printf ("\n\n\n%s", cadena);
 	fflush (stdin);
 	getch ();
 	return 0;
-}
+} //INT MAIN
+//-----------------------------------------
 
-void letras (int letra) {
-	int i, j;
+
+void letras (int unsigned letra[t], char unsigned cadena[t]) {
+	int i, j, k , auxi, mat[5][5];
+	char unsigned var;
+	
+	k=0;
+	auxi=0;
+	do {  //bucle de cadena
+		for (i=auxi; i<5; i=auxi) { //avance en filas de la matriz
+			for (j=0; j<5; j++) { //avance en columnas de la matriz
+				if (cadena[k]=='\0') {
+					auxi++;
+					k=0;
+					printf ("\n");
+					break;
+				}
+				var=cadena[k];
+				switches (var, mat, i);
+				imprimir (mat, i, j);
+				
+				if (j==4) {
+					printf ("   ");
+					k++;
+				}
+			} //fin columnas
+		}  //fin filas
+	} while (auxi!=5); //fin bucle cadena
+} //VOID LETRAS
+//-----------------------------------------
+
+
+void imprimir (int mat[5][5], int i, int j) {
+	if (mat[i][j]==0) {
+		printf (" ");
+	}
+	if (mat[i][j]==1) {
+		printf ("%c", 250);
+	}
+	if (mat[i][j]==2) {
+		printf ("%c", 238);
+	}
+	if (mat[i][j]==3) {
+		printf ("%c", 92);
+	}
+	if (mat[i][j]==4) {
+		printf ("/");
+	}
+} //VOID IMPRIMIR
+//-----------------------------------------
+
+
+void switches (char unsigned var, int mat[5][5], int i) {
 	int A[5][5]={{0, 0, 1, 0, 0},
 				 {0, 1, 0, 1, 0},
 				 {0, 1, 1, 1, 0},
@@ -148,12 +244,12 @@ void letras (int letra) {
 				 {0, 1, 1, 3, 0}};
 	
 	int S[5][5]={{0, 0, 1, 1, 0},
-				 {0, 1, 0, 1, 0},
+				 {0, 1, 0, 0, 0},
 				 {0, 1, 1, 0, 0},
 				 {0, 0, 0, 1, 0},
 				 {0, 1, 1, 0, 0}};
 				 
-	int t[5][5]={{0, 1, 1, 1, 0},
+	int T[5][5]={{0, 1, 1, 1, 0},
 				 {0, 0, 1, 0, 0},
 				 {0, 0, 1, 0, 0},
 				 {0, 0, 1, 0, 0},
@@ -193,27 +289,125 @@ void letras (int letra) {
 				 {0, 0, 0, 1, 0},
 				 {0, 0, 1, 0, 0},
 				 {0, 1, 0, 0, 0},
-				 {0, 1, 1, 1, 0}};
-	for (i=0; i<5; i++) {
-		for (j=0; j<5; j++) {
-			if (W[i][j]==0) {
-				printf  (" ");
-			}
-			if (W[i][j]==1) {
-				printf ("%c", 250);
-			}
-			if (W[i][j]==2) {
-				printf ("%c", 238);
-			}
-			if (W[i][j]==3) {
-				printf ("%c", 92);
-			}
-			if (W[i][j]==4) {
-				printf ("/");
-			}
-		}
-		printf ("\n");
+				 {0, 1, 1, 1, 0}};	
+	
+	switch (var) {
+		case 'A':
+			copiarmat (A, mat, i);
+			break;
+		
+		case 'B':
+			copiarmat (B, mat, i);
+			break;
+			
+		case 'C':
+			copiarmat (C, mat, i);
+			break;
+			
+		case 'D':
+			copiarmat (D, mat, i);
+			break;
+		
+		case 'E':
+			copiarmat (E, mat, i);
+			break;
+		
+		case 'F':
+			copiarmat (F, mat, i);
+			break;
+			
+		case 'G':
+			copiarmat (G, mat, i);
+			break;
+			
+		case 'H':
+			copiarmat (H, mat, i);
+			break;
+		
+		case 'I':
+			copiarmat (I, mat, i);
+			break;
+			
+		case 'J':
+			copiarmat (J, mat, i);
+			break;
+			
+		case 'K':
+			copiarmat (K, mat, i);
+			break;
+		
+		case 'L':
+			copiarmat (L, mat, i);
+			break;
+			
+		case 'M':
+			copiarmat (M, mat, i);
+			break;
+			
+		case 'N':
+			copiarmat (N, mat, i);
+			break;
+		
+		case 165:
+			copiarmat (N1, mat, i);
+			break;
+			
+		case 'O':
+			copiarmat (O, mat, i);
+			break;
+			
+		case 'P':
+			copiarmat (P, mat, i);
+			break;
+		
+		case 'Q':
+			copiarmat (Q, mat, i);
+			break;
+			
+		case 'R':
+			copiarmat (R, mat, i);
+			break;
+			
+		case 'S':
+			copiarmat (S, mat, i);
+			break;
+		
+		case 'T':
+			copiarmat (T, mat, i);
+			break;
+			
+		case 'U':
+			copiarmat (U, mat, i);
+			break;
+			
+		case 'V':
+			copiarmat (V, mat, i);
+			break;
+		
+		case 'W':
+			copiarmat (W, mat, i);
+			break;
+			
+		case 'X':
+			copiarmat (X, mat, i);
+			break;
+			
+		case 'Y':
+			copiarmat (Y, mat, i);
+			break;
+		
+		case 'Z':
+			copiarmat (Z, mat, i);
+			break;
 	}
-	fflush (stdin);
-	system ("pause");
-}
+} //VOID SWITCHES
+//-----------------------------------------
+
+
+void copiarmat (int original[5][5], int copia[5][5], int i) {
+	int j;
+	for (j=0; j<5; j++) {
+		copia[i][j]=original[i][j];
+	}
+} //VOID COPIARMAT
+//-----------------------------------------
